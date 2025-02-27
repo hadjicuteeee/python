ffa5ff658c52cedff963861f1019515bc9902475
Jihad Dion <144666381+hadjicuteeee@users.noreply.github.com>
2025-02-28 03:51:50 +0800

    Add files via upload

---
+++ inputuser/input.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE inputDB;
+
+CREATE TABLE tblinputuser(
+id INT PRIMARY KEY auto_increment,
+first_name VARCHAR(20) NOT NULL,
+last_name VARCHAR(20) NOT NULL,
+course VARCHAR(20) NOT NULL,
+age int);
+
---
+++ inputuser/inputuser.py
@@ -0,0 +1,44 @@
+import mysql.connector
+
+
+def insert_data(first_name, last_name, course, age):
+    try:
+        conn = mysql.connector.connect(
+        host = "localhost",
+        user="root",
+        password = "",
+        database = "input_user"
+        )
+        cursor = conn.cursor()
+
+        query = "INSERT INTO tblinputuser (first_name, last_name, course, age) VALUES (%s, %s, %s, %s)"
+
+        values = (first_name, last_name, course, age)
+        cursor.execute(query, values)
+        conn.commit()
+        conn.close()
+
+
+
+
+
+        print("Data Successfully Inserted")
+
+    except Exception as e:
+        print("Not successfully", str(e))
+
+
+
+
+
+
+
+
+while True:
+    first_name = input("Enter your name: ")
+    last_name = input("Enter your last name : ")
+    course = input("Enter your course : ")
+    age = input("Enter your age : ")
+
+    insert_data(first_name, last_name, course, age)
+
